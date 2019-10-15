d83171106e27fb51762b08b5396ee6ae0e6c9fb4
wifidu <875147715@qq.com>
2019-10-15 19:51:08 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,16 @@
 
 
 @section('content')
- <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      你现在看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a> 杜韦凡的主页。
+    </p>
+    <p>
+      一切将从这里开始。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  <div>
 @stop
