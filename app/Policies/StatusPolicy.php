d198c88a2c59f18b8fb49438d551d90aa1e19b10
wifidu <875147715@qq.com>
2019-10-20 15:10:52 +0000

    今天做到『删除微博』明天继续，记得复习

---
+++ app/Policies/StatusPolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class StatusPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function destory(User $user, Status $status)
+    {
+        return $user->id === $status->user_id;
+    }
+}
