5fc18d4d65973363eb8088c6927e7e890a35eea2
wifidu <875147715@qq.com>
2019-10-21 14:32:58 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,11 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                    ->with('user')
+                    ->orderBy('created_at', 'desc');
     }
 
 
@@ -97,4 +101,5 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
 }
